ddefb3917877c4c09356b7b5d9fec361ff81a74d
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-08-19 14:27:18 -0400

    production build

--- utils/index.tsx
+++ utils/index.tsx
@@ -18,4 +18,4 @@ const isDev = process.env.NODE_ENV === "development"
 
 export const ORIGIN_URL = isDev
     ? "http://localhost:3000"
-    : "https://open-source-spirit-shop.vercel.app"
+    : "https://spirit-shop.vercel.app"
